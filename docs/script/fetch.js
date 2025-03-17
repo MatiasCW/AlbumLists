074235b822a093d6bb15f88d56d63d5c8ad61228
MatiasCW <109916599+MatiasCW@users.noreply.github.com>
2025-03-17 14:10:56 -0400

    Update fetch.js

--- docs/script/fetch.js
+++ docs/script/fetch.js
@@ -1,5 +1,5 @@
 import { auth, db } from "./firebase.js";
-import { collection, addDoc, getDocs, query, where, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, query, where, doc, deleteDoc, setDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 const clientId = '2b46bd9e8aef47908b9b92deac88846b';
 const clientSecret = '774c6a6cf06b4595ad2bb6f776d8ff23';
