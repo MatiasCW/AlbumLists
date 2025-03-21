ef54033d1440da3ccaf8ac2b17642b795b045e74
MatiasCW <109916599+MatiasCW@users.noreply.github.com>
2025-03-21 18:49:41 -0400

    Update profile.js

--- docs/script/profile.js
+++ docs/script/profile.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./script/firebase.js";
+import { auth, db } from "./firebase.js";
         import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
         import { doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
